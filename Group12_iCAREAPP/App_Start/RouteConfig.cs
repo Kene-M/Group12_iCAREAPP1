d677260cf4418ad0e67faf192a71239e00b5f84a
Sydney Poulsen <snpbx7@umsystem.edu>
2024-10-24 13:48:17 -0500

    Created Login Page

--- Group12_iCAREAPP/App_Start/RouteConfig.cs
+++ Group12_iCAREAPP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Group12_iCAREAPP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Group12_iCAREAPP/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Group12_iCAREAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Group12_iCAREAPP.Controllers
+{
+    public class LoginController : Controller
+    {
+        private Group12_iCAREDBEntities db = new Group12_iCAREDBEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            //if password is right and id in admin (get UserRoles) -> AdminView
+            //if password is right and id in nurse -> NurseView
+            //if password is right and id in doctor -> DoctorView
+            //else "credentials are incorrect try again"
+    
+            return View();
+        }
+    }
+}
