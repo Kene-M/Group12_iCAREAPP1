afbd36c8c6669e0e9424f6817d581102c566dd0f
Sydney Poulsen <snpbx7@umsystem.edu>
2024-10-28 17:16:24 -0500

    edited login

--- Group12_iCAREAPP/Controllers/LoginController.cs
+++ Group12_iCAREAPP/Controllers/LoginController.cs
@@ -1,9 +1,10 @@
-﻿using Group12_iCAREAPP.Models;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.Data.Entity;
+using System.Drawing.Printing;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Group12_iCAREAPP.Models;
 
 namespace Group12_iCAREAPP.Controllers
 {
@@ -12,14 +13,38 @@ namespace Group12_iCAREAPP.Controllers
         private Group12_iCAREDBEntities db = new Group12_iCAREDBEntities();
 
         // GET: Login
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Login()
         {
-            //if password is right and id in admin (get UserRoles) -> AdminView
-            //if password is right and id in nurse -> NurseView
-            //if password is right and id in doctor -> DoctorView
-            //else "credentials are incorrect try again"
-    
             return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+                var user =  db.iCAREUser.Where(u => u.name.Equals(username)).FirstOrDefault();
+
+                if (user != null)
+                {
+                    //find the password
+                    var userPassword = db.UserPassword.Where(p => p.ID.Equals(user.passwordID)).FirstOrDefault();
+
+                    if (userPassword != null)
+                    {
+                        //compare passwords
+                        if (password == userPassword.password) 
+                        {
+                            if(user.ID=="admin")
+                            {
+                                return RedirectToAction("Index", "iCAREWorkers");
+                            }
+                            return RedirectToAction("Index", "PatientRecords");
+                        }
+                    }
+                }
+
+            return View("Login", username, password);
         }
     }
 }
